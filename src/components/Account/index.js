0875b32022b5e18edae09292fbbd70650ab9ff5e
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-12 17:58:03 -0800

    Appease the lint gods

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -16,7 +16,7 @@ class AccountComponent extends Component {
         <div className='profile-header'>
           <h1 className='title is-3'>{ this.props.authenticatedUser.displayName }</h1>
           <div class='image is-64x64'>
-            <img className='profile-photo is-rounded' src={this.props.authenticatedUser.photoURL} />
+            <img className='profile-photo is-rounded' src={this.props.authenticatedUser.photoURL} alt='Firebase notes profile'/>
           </div>
         </div>
 
