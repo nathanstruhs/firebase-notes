85219a59a7b9eeaad8e0761324db7a57be2de759
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-12 17:45:46 -0800

    Notes screen layout markup and styles

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -6,7 +6,32 @@ import './index.scss';
 class Notes extends Component {
   render() {
     return(
-      <div></div>
+      <div className='columns'>
+      <div className='column has-background-info is-one-third'>
+
+        <aside className='menu'>
+          <div className='sidebar-header-container'>
+            <h2 className='menu-label title is-3'>Ya Notes Boy</h2>
+            <button className='button is-info is-inverted'>New Note +</button>
+          </div>
+
+          <hr />
+
+        </aside>
+      </div>
+
+      <div className='column has-background-info'>
+        <div className='note-title title is-4'>
+          Title
+        </div>
+
+        <hr />
+
+        <div className='note-body'>
+          Body
+        </div>
+      </div>
+    </div>
     )
   }
 }
